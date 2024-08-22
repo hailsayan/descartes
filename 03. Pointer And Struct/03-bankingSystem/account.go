8610c535e1f5540648f1a1a69ced2b6902aff892
Psyon <sayan.h2020@gmail.com>
2024-08-22 11:05:19 +0330

    test

--- 03. Pointer And Struct/03-bankingSystem/account.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Account interface {
-	MonthlyInterest() int
-	Transfer(receiver Account, amount int) string
-	Deposit(amount int) string
-	Withdraw(amount int) string
-	CheckBalance() int
-}
--- 11. Database Review/02-gotOne-R-InIt/queries.sql
+++
@@ -1,6 +0,0 @@
--- Section1
-    DELETE FROM users WHERE family NOT LIKE '%m%' AND family NOT LIKE '%d%';
--- Section2
-    DELETE FROM users WHERE family = 'mohammadi' OR salary IN (7356, 9701, 2885, 7414, 3801);
--- Section3
-    DELETE FROM users WHERE family = 'booazar' OR YEAR(birth_date) BETWEEN 1995 AND 2000;  
